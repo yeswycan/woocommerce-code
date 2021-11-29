c79655d91e2496ccdc605c83abcd58746e0f8768
WYCAN <82756397+yeswycan@users.noreply.github.com>
2021-11-29 12:06:37 +0100

    Update add-yotpo-stars-ratings-elementor-template.php

--- add-yotpo-stars-ratings-elementor-template.php
+++ add-yotpo-stars-ratings-elementor-template.php
@@ -9,3 +9,11 @@ if ( function_exists( 'wc_yotpo_show_buttomline' ) )
 { add_action( 'woocommerce_single_product_summary', 'wc_yotpo_show_buttomline', 15 ); }
 }
 add_shortcode( 'wycan_yotpo_stars_rating', 'wc_yotpo_show_buttomline');
+
+
+// Add the reviews area inside a single product template with elementor, simply add the shortcode widget with [wycan_yotpo_reviews_rating]//
+function wycan_yotpo_reviews_single( $atts ) {
+if ( function_exists( 'wc_yotpo_show_widget' ) ){
+add_action( 'woocommerce_after_single_product', 'wc_yotpo_show_widget', 15 ); }
+}
+add_shortcode( 'wycan_yotpo_reviews_rating', 'wc_yotpo_show_widget');
